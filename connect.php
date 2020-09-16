96b05c854bf67d914a08376bf608b801931aa033
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-16 23:02:53 +0700

    select and order data from table MyGuests

--- connect.php
+++ connect.php
@@ -11,7 +11,7 @@
         die("Connection failed: ".$conn->connect_error);
     }
     
-    $sql = "SELECT id, firstname, lastname FROM MyGuests WHERE lastname ='Doe'";
+    $sql = "SELECT id, firstname, lastname FROM MyGuests ORDER BY lastname";
     $result = $conn->query($sql);
 
     if($result->num_rows >0){
