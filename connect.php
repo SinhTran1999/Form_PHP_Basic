00906636b25bcb4aec52aa0c30731be0ec73813d
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-16 20:38:28 +0700

    create a database in MySQL

--- connect.php
+++ connect.php
@@ -9,5 +9,12 @@
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
     }
-    echo "Connected successfully";
+    //Create database
+    $sql = "CREATE DATABASE myDB";
+    if($conn->query($sql) === TRUE){
+        echo "Database created successfully";
+    }else{
+        echo "Error creating database: ".$conn->error;
+    }
+    $conn->close();
 ?>
